57dd0fc30edb9e59d4c1abfbd09cf8f4faa83d2f
Alexander Evstratov <alexdrugev@me.com>
2024-03-25 19:28:13 +0300

    Fix endless background.

--- Assets/_Scripts/EndlessBackground.cs
+++ Assets/_Scripts/EndlessBackground.cs
@@ -4,12 +4,12 @@ public class EndlessBackground : MonoBehaviour
 {
     [SerializeField] private float _speed = 2.0f;
 
-    private void Update()
+    private void FixedUpdate()
     {
         transform.Translate(Vector2.down * Time.deltaTime * _speed);
-        if (transform.position.y <= -19.8f)
+        if (transform.position.y <= -12f)
         {
-            transform.position = new Vector2(0, 19.8f);
+            transform.position = new Vector2(0, 39.1f);
         }
     }
 }
