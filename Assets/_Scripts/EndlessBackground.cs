6d46940e642d4ee433574f14d6f7322af71cacd6
Alexander Evstratov <alexdrugev@me.com>
2024-03-25 19:49:23 +0300

    Change tutorial panel.

--- Assets/_Scripts/EndlessBackground.cs
+++ Assets/_Scripts/EndlessBackground.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class EndlessBackground : MonoBehaviour
 {
-    [SerializeField] private float _speed = 2.0f;
+    [SerializeField] private GameObject _tutorial;
+    private float _speed = 2.0f;
 
     private void FixedUpdate()
     {
-        transform.Translate(Vector2.down * Time.deltaTime * _speed);
-        if (transform.position.y <= -12f)
+        if (!_tutorial.activeInHierarchy)
         {
-            transform.position = new Vector2(0, 39.1f);
+            transform.Translate(Vector2.down * Time.deltaTime * _speed);
+            if (transform.position.y <= -12f)
+            {
+                transform.position = new Vector2(0, 39.1f);
+            }
         }
     }
 }
--- Assets/_Scripts/EnterGame.cs
+++ Assets/_Scripts/EnterGame.cs
@@ -6,14 +6,8 @@ public class EnterGame : MonoBehaviour
 {
     [SerializeField] private GameObject _tutorial;
 
-    private void Start()
-    {
-        Time.timeScale = 0;
-    }
-
     public void GoBtn()
     {
         _tutorial.SetActive(false);
-        Time.timeScale = 1;
     }
 }
